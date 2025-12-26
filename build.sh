8f2e0244cf26c2f8d6c474d2b9efdaa0ecfb572e
papagi <cszy12345690@gmail.com>
2025-12-26 10:38:16 +0800

    ver5.0 first commit

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 set -ex
 #set registry
-USERNAME=129729052534.dkr.ecr.ap-northeast-1.amazonaws.com
+USERNAME=754515738597.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
-IMAGE=iii-devops-ecr
+IMAGE=cjc102-10-ecr-repo
 docker build -t $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -p 5000:5000 --name flask-devops -d 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/iii-devops-ecr:latest
+docker run -p 80:5000 --name flask-web -d 754515738597.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-10-ecr-repo:latest
+
+sleep 20s
